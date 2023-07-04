7ee7ed0d599a1d802cde59306012628184916535
rodric74 <rodric.rambosson@gmail.com>
2023-07-04 21:52:22 +0200

    tentative purge css

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['index.html'],
-  css: ['assets/bootstrap/bootstrap.min.css','assets/style.css'],
+  css: ['assets/bootstrap/bootstrap.css'],
   keyframes: true,
   output: '/Users/roromac/Documents/KreaWork/WEB/P05/nina-carducci.github.io/assets/clean/',
   rejected: true,
